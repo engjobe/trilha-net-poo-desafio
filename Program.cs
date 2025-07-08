2ea309eb3dde61407dae1ad4daf9a57a252497b1
Joao Guidugli <silverockhopper@gmail.com>
2025-07-08 10:37:19 -0300

    update código

--- Program.cs
+++ Program.cs
@@ -1,3 +1,21 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+// Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main()
+    {
+        Smartphone nokia = new Nokia("11555555555", "Nokia G11 Plus", "1234567890", 256);
+        Smartphone iphone = new Iphone("11777777777", "iPhone 12", "0987654321", 128);
+
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("Facebook");
+
+        Console.WriteLine(); // pular uma linha
+
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+    }
+}
